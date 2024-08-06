52f9cef22f676e3b3858f80bae8a53e907c90641
sanjeev <sanjeevgaikwad90@gmail.com>
2024-08-05 23:22:24 -0600

    pyramid making

---
+++ PyramidPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PyramidPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
