5bc5308ea0d2ec1e2f549fb6f1dbaf0875bbf24e
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-08 00:35:11 -0800

    UpdateMarks.sql
    updating marks for one wrong question in exam

---
+++ UpdateMarks.sql
@@ -0,0 +1,5 @@
+-- Practical situation now professor realised that there was one wrong question in question paper and hence everyone 1 marks must be raised. 
+
+UPDATE Student
+SET Marks = Marks + 1;
+SELECT * FROM Student;
