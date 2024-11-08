acdc7e4287ca029465c3c15dd078523406120c2f
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-08 00:32:46 -0800

    Update.sql
    How to easily update tables by turning off safe mode

---
+++ Update.sql
@@ -0,0 +1,3 @@
+-- Sometimes we may get an warning from sql about safe mode on : preventing not to update unnecessary changes in table. 
+-- We can off it by : 
+SET SQL_SAFE_UPDATES = 0;      1 is on 0 is off
