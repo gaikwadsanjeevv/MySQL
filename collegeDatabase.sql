961bf9f9af15341012b81d99c6d166679cd3a317
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-08 00:27:13 -0800

    collegeDatabase.sql
    college database

---
+++ collegeDatabase.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE college;
+use college;
+
+
+CREATE TABLE student11 (
+rollno INT PRIMARY KEY,
+name varchar(50),
+marks INT NOT NULL,
+grade varchar(1),
+city varchar(20)
+);
+INSERT INTO student11 (rollno, name, marks, grade, city)
+values
+(101, "anil", 78, "C", "Pune"),
+(102, "bhumika", 93, "A", "Mumbai"),
+(103, "chetan", 85, "B", "Mumbai"),
+(104, "dhruv", 96, "A", "Delhi"),
+(105, "emanuel", 12, "F", "Delhi"),
+(106, "farah", 82, "B", "Delhi");
+
+
+select * from student11;
